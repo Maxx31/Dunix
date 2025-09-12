a53670e11bacddb7cf81aca8e2480f5c1f5dd1db
Maxim213 <133004196+Maxim213@users.noreply.github.com>
2025-09-12 16:19:48 +0300

    Proper layer pushing

--- Dunix/src/Dunix/Core/LayerStack.cpp
+++ Dunix/src/Dunix/Core/LayerStack.cpp
@@ -4,13 +4,19 @@ namespace Dunix
 {
 	void LayerStack::PushLayer(std::unique_ptr<Layer>inLayer)
 	{
-		inLayer->OnAttach();
+		Layer* ptr = inLayer.get();
+		m_Layers.insert(m_Layers.begin() + static_cast<std::ptrdiff_t>(m_LastLayerIndex),
+			std::move(inLayer));
+
+		m_LastLayerIndex++;
+		ptr->OnAttach();
 	}
 
 	void LayerStack::PushOverlay(std::unique_ptr<Layer>inOverlay)
 	{
-		m_Layers.emplace_back(inOverlay);
-		inOverlay->OnAttach();
+		Layer* ptr = inOverlay.get();
+		m_Layers.emplace_back(std::move(inOverlay));
+		ptr->OnAttach();
 	}
 
 	std::unique_ptr<Layer> LayerStack::PopLayer(Layer* inLayer)
