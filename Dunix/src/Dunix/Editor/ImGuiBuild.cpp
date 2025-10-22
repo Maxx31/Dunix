f9b159bebb0a52d9ccdf12a1f70f0d67bf4ab149
Maxim213 <133004196+Maxim213@users.noreply.github.com>
2025-10-22 10:35:02 +0300

    Fixed compilation error

--- Dunix/src/Dunix/Editor/ImGuiBuild.cpp
+++ Dunix/src/Dunix/Editor/ImGuiBuild.cpp
@@ -1,13 +1,6 @@
 #include "dxpch.h"
 
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#define IMGUI_DEFINE_MATH_OPERATORS   
 
 #include "backends/imgui_impl_opengl3.cpp"
 #include "backends/imgui_impl_glfw.cpp"
-
-#include "imgui.cpp"
-#include "imgui_demo.cpp"
-#include "imgui_draw.cpp"
-#include "imgui_tables.cpp"
-#include "imgui_widgets.cpp"
