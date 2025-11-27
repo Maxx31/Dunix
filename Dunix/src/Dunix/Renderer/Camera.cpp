98fab119f7d22b8c0b1ecc1040abefbfff5765f5
Maxim213 <133004196+Maxim213@users.noreply.github.com>
2025-11-27 17:59:43 +0200

    Basic Camera script

---
+++ Dunix/src/Dunix/Renderer/Camera.cpp
@@ -0,0 +1,18 @@
+#include "dxpch.h"
+#include "Camera.h"
+
+namespace Dunix
+{
+	Camera::Camera(float FOV, float aspect, float nearClip, float farClip)
+	{
+
+	}
+
+	void Camera::SetPosition(const glm::vec3& pos)
+	{
+	}
+
+	void Camera::SetRotation(const glm::vec3& rotEuler)
+	{
+	}
+}
---
+++ Dunix/src/Dunix/Renderer/Camera.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Dunix
+{
+	class Camera
+	{
+	public:
+		Camera(float FOV, float aspect, float nearClip, float farClip);
+
+		void SetPosition(const glm::vec3& pos);
+		void SetRotation(const glm::vec3& rotEuler);
+
+		const glm::mat4& GetView() const { return m_View; }
+		const glm::mat4& GetProjection() const { return m_Projection; }
+		glm::mat4 GetViewProjection() const { return m_Projection * m_View; }
+
+	private:
+		float m_FOV, m_Aspect, m_nearClip, m_farClip;
+
+		glm::vec3 m_Position;
+		glm::vec3 m_Rotation; // pitch, yaw, roll
+
+		glm::mat4 m_View{ 1.0f };
+		glm::mat4 m_Projection{ 1.0f };
+
+	};
+}
+
